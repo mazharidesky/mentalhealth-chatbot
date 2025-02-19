2ebc5c26efb99b9f6c9cbaf0be78f2f5bda4bd3e
mazharidesky <muhammad.210170230@mhs.unimal.ac.id>
2025-02-19 13:48:25 +0700

    feat: front-end in production

--- production-chatbot/app.py
+++ production-chatbot/app.py
@@ -2,12 +2,44 @@ from flask import Flask, render_template, request, jsonify
 from chatbot import ContextualMentalHealthChatbot
 import os
 
+
+data = [
+    {
+        "image": "../static/img/psikiater-1.png",
+        "name": "Arman Sp.Kj",
+        "specialist": "Konsultan Kesehatan Jiwa",
+        "hubungi": "08123456789"
+    },
+    
+    {
+        "image": "../static/img/psikiater-1.png",
+        "name": "Arman Sp.Kj",
+        "specialist": "Konsultan Kesehatan Jiwa",
+        "hubungi": "08123456789"
+    },
+    
+    {
+        "image": "../static/img/psikiater-1.png",
+        "name": "Arman Sp.Kj",
+        "specialist": "Konsultan Kesehatan Jiwa",
+        "hubungi": "08123456789"
+    },
+    
+    {
+        "image": "../static/img/psikiater-1.png",
+        "name": "Arman Sp.Kj",
+        "specialist": "Konsultan Kesehatan Jiwa",
+        "hubungi": "08123456789"
+    },
+    
+]
+
 app = Flask(__name__, static_folder='static')
 chatbot = ContextualMentalHealthChatbot('data.json')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', data=data)
 
 @app.route('/chat', methods=['POST'])
 def chat():
